b846a2395f5f41b489def4fa280ff91ecd41ae0b
Borodinskiy88 <a-borodinskiy@mail.ru>
2023-01-29 18:08:39 +0300

    glide

--- app/src/main/java/ru/netology/nmedia/adapter/PostsAdapter.kt
+++ app/src/main/java/ru/netology/nmedia/adapter/PostsAdapter.kt
@@ -1,11 +1,13 @@
 package ru.netology.nmedia.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import ru.netology.nmedia.R
 import ru.netology.nmedia.databinding.CardPostBinding
 import ru.netology.nmedia.dto.Post
@@ -36,12 +38,23 @@ class PostViewHolder(
     private val onInteractionListener: OnInteractionListener,
 ) : RecyclerView.ViewHolder(binding.root) {
 
+    private val avatarUrl = "http://10.0.2.2:9999/avatars/"
+    private val attachmentUrl = "http://10.0.2.2:9999/images/"
+
     fun bind(post: Post) {
         binding.apply {
             author.text = post.author
+
+            Glide.with(avatar)
+                    .load(avatarUrl + post.authorAvatar)
+                    .circleCrop()
+                    .placeholder(R.drawable.ic_loading_100dp)
+                    .error(R.drawable.ic_error_100dp)
+                    .timeout(10_000)
+                    .into(avatar)
+
             published.text = post.published
             content.text = post.content
-            // в адаптере
             like.isChecked = post.likedByMe
             like.text = "${post.likes}"
 
@@ -71,6 +84,15 @@ class PostViewHolder(
 
             share.setOnClickListener {
                 onInteractionListener.onShare(post)
+            }
+
+            if (post.attachment != null) {
+                attachment.visibility = View.VISIBLE
+                attachment.contentDescription = post.attachment.description
+                Glide.with(attachment)
+                    .load(attachmentUrl + post.attachment.url)
+                    .timeout(10_000)
+                    .into(binding.attachment)
             }
         }
     }
--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -3,9 +3,17 @@ package ru.netology.nmedia.dto
 data class Post(
     val id: Long,
     val author: String,
+    val authorAvatar: String,
     val content: String,
     val published: String,
-    val likedByMe: Boolean,
     val likes: Int = 0,
+    val likedByMe: Boolean,
+    val attachment: Attachment? = null
 )
+
+data class Attachment (
+    val url: String,
+    val description: String,
+    val type: String
+        )
 
